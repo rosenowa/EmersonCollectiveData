b403b4963a0cf84f28a53d6c633942a0431d25cf
arosenow <arosenow@urban.org>
2023-10-23 18:40:30 -0400

    Updated Gabbi's code

--- DataforGabbi.R
+++ DataforGabbi.R
@@ -3,37 +3,23 @@ library(tidyverse)
 library(tidycensus)
 library(sf)
 library(urbnthemes)
+library(dplyr)
 
 # update census API key with your personal key: https://api.census.gov/data/key_signup.html
-#Annie's census api key:('25e7c711e84c77deb722fd007f9152d745a46027')
-#census_api_key('2330d2d66e60a29868283ad737077ca35e502983')
+#census_api_key('063c3c50651249b77ec616eec973ae7ab5fde4dc')
+acs_data <- load_variables(year = 2021,
+                                dataset = "acs5", 
+                           cache = TRUE) #data detail tables
+head(acs_data)
 
-# load ACS variables to identify the correct variable names for analysis 
-acs21 <- load_variables(2021, "acs5", cache = TRUE)
-View(acs21)
+acs_data[grep(x = acs_data$concept, "Sex"),
+         c("concept", "label")]
 
-####-----------------pull acs data for vars at dif geos--------------------####
-
-
-acs_pull_gabbi <- function(geos, years, block_group_vars_gabbi){
-  get_acs(geography = geos,
-          year = years, 
-          variables = selected_vars,
-          geometry = TRUE, # include geometry for mapping
-          state = "DC") %>% 
-    mutate(year = years)}
-  
-  ### -----------------------call---------------------------------####
-  
-  # study years to iterate over
-  # years <- c(2012:2022)
-  years <- c(2021)
-  
-  
-  #--------block group--------#
-  geos <- "block group"
-  
-  block_group_vars_gabbi <- c("B01001_003", #total M under 5 years
+ch_acs_detail <- get_acs(geography = "tract",
+          state = "DC",
+          county = "001",
+          year = 2021,
+          variables= c( "B01001_003", #total M under 5 years
                         "B01001_004", #total M 5-9 years
                         "B01001_005", #total M 10-14 years
                         "B01001_006", #total M 15 to 17 years
@@ -56,7 +42,6 @@ acs_pull_gabbi <- function(geos, years, block_group_vars_gabbi){
                         "B01001_023", #total M 75 to 79 years
                         "B01001_024", #total M 80 to 84 years
                         "B01001_025", #M pop over 85
-                        #AGE BY FEMALE
                         "B01001_027", #total F under 5 years
                         "B01001_028", #total F 5-9 years
                         "B01001_029", #total F 10-14 years
@@ -80,75 +65,126 @@ acs_pull_gabbi <- function(geos, years, block_group_vars_gabbi){
                         "B01001_047", #total F 75 to 79 years
                         "B01001_048", #total F 80 to 84 years
                         "B01001_049", #F pop over 85
-                        #median age
                         "B01002_002", #median age M
-                        "B01002_002", #median age F
-                        #total population count by sex
+                        "B01002_003", #median age F
                         "B01003_001", #total pop
-                        "B01001_002", #total M 
+                        "B01001_002", #total M
                         "B01001_026", #total F
-                        "B03002_001", #total race pop.    
-                        "B03002_002", #total nH 
+                        "B03002_001", #total race pop.
+                        "B03002_002", #total nH
                         "B03002_003", #total nH white
                         "B03002_004", #total nH Black
                         "B03002_005", #total nH AmerInd NativeAmer
                         "B03002_006", #total nH asian
                         "B03002_007", #toal nH NativeHawaiian PacificIslander
-                        "B03002_012" #total Hisp
-                        )
+                        "B03002_012", #total Hisp
+                        "B07013_003",#renters
+                        "B07013_002" #owners
+          ), 
+          geometry = T,
+          output= "wide")
+
+#filtering it so that it only shows congress height tracks
+ch_acs_detail <- filter(ch_acs_detail, GEOID==11001009804 | GEOID==11001007304 | GEOID==11001010400) 
+
+#dropping Margin of Error 
+ch_acs_detail <-ch_acs_detail %>% select(ends_with("E"))
+
+#grouping variables and creating new names
+ch_acs_detail <-ch_acs_detail %>% mutate(
+                                         under5 = B01001_003E + B01001_027E,
+                                         pop5_17 = B01001_004E + B01001_005E + B01001_006E + 
+                                           B01001_028E + B01001_029E + B01001_030E,
+                                         pop18_24 =
+                                           B01001_007E + B01001_008E + B01001_009E + B01001_010E +
+                                           B01001_031E + B01001_032E + B01001_033E + B01001_034E,
+                                         pop25_65 =
+                                           B01001_011E + B01001_012E + B01001_013E + B01001_014E + B01001_015E +
+                                           B01001_016E + B01001_017E + B01001_018E + B01001_019E +
+                                           B01001_035E + B01001_036E + B01001_037E + B01001_038E + B01001_039E +
+                                           B01001_040E + B01001_041E + B01001_042E + B01001_043E,
+                                         pop65_85 =
+                                           B01001_020E + B01001_021E + B01001_022E + B01001_023E + B01001_024E +
+                                           B01001_044E + B01001_045E + B01001_046E + B01001_047E + B01001_048E,
+                                         popover85 = B01001_025E + B01001_049E,
+                                         male_tract= B01001_002E, 
+                                         female_tract= B01001_026E,
+                                         total_race = B03002_001E,
+                                         white_alone = B03002_003E, 
+                                         black = B03002_004E,
+                                         amind_natalas = B03002_005E,
+                                         asian = B03002_006E, 
+                                         nathi_pi = B03002_007E,
+                                         hisp = B03002_012E,
+                                         median_ageM = B01002_002E,
+                                         median_ageF = B01002_003E,
+                                         renters = B07013_003E,
+                                         owners = B07013_002E)
+# #ch_acs_detail <-ch_acs_detail %>% mutate(white_perc = white_alone / total_race,
+#                                          black_perc = black / total_race,
+#                                          amind_perc = amind_natalas / total_race,
+#                                          asian_perc = asian / total_race,
+#                                          nathi_perc = nathi_pi / total_race,
+#                                          hisp_perc = hisp / total_race)
+
+# #I want to create one row that summarizes everything but have yet to do that
+# summarise (ch_acs_detail)
+# #trying to add a new column to represent the total amount across tracts
+# CH_total %>%
+#   bind_rows(summarise_all(ch_acs_detail, ~if(is.numeric(.)) sum(.) else "Total")
+
+
+# creating age table
+ch_acs_AGE <- ch_acs_detail %>% 
+  select(under5,pop5_17,pop18_24,pop25_65,pop65_85,popover85) %>% 
+  summarize(totalunder5 =sum(under5),
+            totalpop5_17 = sum(pop5_17),
+            totalpop18_24 = sum(pop18_24),
+            totalpop25_65 = sum(pop25_65),
+            totalpop65_85 = sum(pop65_85),
+            totalpopover85 = sum(popover85))%>% 
+  summarize(perc_under5= totalunder5/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85), 
+            perc_5_17= totalpop5_17/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85), 
+            perc_18_24= totalpop18_24/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85),
+            perc_25_65= totalpop25_65/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85),
+            perc_65_85= totalpop65_85/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85),
+            perc_over85= totalpopover85/ (totalunder5 + totalpop5_17 + totalpop18_24 + totalpop25_65 + totalpop65_85 + totalpopover85) 
+            )
+            
+# creating F and M table
+ch_acs_SEX <- ch_acs_detail %>% 
+  select(male_tract, female_tract) %>% 
+  summarize(totalMpop =sum(male_tract),
+            totalFpop = sum(female_tract)) %>% 
+  summarize(perc_male= totalMpop/(totalMpop + totalFpop),
+            perc_female= totalFpop/(totalMpop + totalFpop)) 
   
-  
-  # function call
-  bg_datagabbi <- map_df(years, ~acs_pull(geos, .x, block_group_vars_gabbi))%>%
-    
-    # here manipulate data and rename
-    pivot_wider(id_cols = c("GEOID", "NAME", "geometry"), # reshaping the data so that it's easier to manipulate 
-                # (feel free to run lines 14-31 before the %>% to see what it looked like)
-                names_from = "variable",
-                values_from = c("estimate", "moe")) %>%
-    mutate(total_pop = estimate_B01003_001, # create new variables for maps; update variable names so that they're recognizable
-           under5_pop = estimate_B01001_003 + estimate_B01001_027, #M + F
-           between5_17 = estimate_B01001_004 + estimate_B01001_005 + estimate_B01001_006 + 
-             estimate_B01001_028 + estimate_B01001_029 + estimate_B01001_030,
-           between18_24 = estimate_B01001_007 + estimate_B01001_008 + estimate_B01001_009 + estimate_B01001_010 +
-             estimate_B01001_031 + estimate_B01001_032 + estimate_B01001_033 + estimate_B01001_034, 
-           between25_65 = 	estimate_B01001_035 + 
-             estimate_B01001_011 + estimate_B01001_012 + estimate_B01001_013 + estimate_B01001_014 + estimate_B01001_015 +
-             estimate_B01001_016 + estimate_B01001_017 + estimate_B01001_018 + estimate_B01001_019 +
-             estimate_B01001_035 + estimate_B01001_036 + estimate_B01001_037 + estimate_B01001_038 + estimate_B01001_039 + 
-             estimate_B01001_040 + estimate_B01001_041 + estimate_B01001_0342 + estimate_B01001_043, 
-           between65_85 = 
-             estimate_B01001_020 + estimate_B01001_021 + estimate_B01001_022 + estimate_B01001_023 + estimate_B01001_024 +
-             estimate_B01001_044 + estimate_B01001_045 + estimate_B01001_046 + estimate_B01001_047 + estimate_B01001_048, 
-           over85 = estimate_B01001_025 + estimate_B01001_049, 
-           total_race = estimate_B03002_001,
-           white_alone = estimate_B03002_003,
-           black = estimate_B03002_004,
-           amind_natalas = estimate_B03002_005,
-           asian = estimate_B03002_006,
-           nathi_pi = estimate_B03002_007,
-           hisp = estimate_B03002_012,
-           white_perc = white_alone / total_race,
-           black_perc = black / total_race,
-           amind_perc = amind_natalas / total_race,
-           asian_perc = asian / total_race,
-           nathi_perc = nathi_pi / total_race,
-           hisp_perc = hisp / total_race
-           )%>%
-    st_transform("ESRI:102003")%>% # set the coordinate reference system (crs) so that all of the map layers are aligned
-  # select(-contains(MOE and estimate))
-  # more info on crs: https://urbaninstitute.github.io
-  
-  ### -----------------------geographies---------------------------------####
-  
-  ##### set geographies
-  # filter out block groups identified as relevant congress heights areas by the interns, plus saint elizabeth's campus
-  # we will use these block groups as a layer in the maps
-  congress_heights_bg <- bg_datagabbi %>%
-    filter(GEOID %in% c("110010098041","110010098042","110010073041",
-                        "110010073042","110010073043","110010104003"))
-  
-  #tracts 104, 73.04, 98.04 - can add others as appropriate
-  parcel15_bg <- bg_datagabbi %>%
-    filter(GEOID %in% c("110010098041","110010098042","110010073041",
-                        "110010073042","110010073043","110010104003"))
+
+# creating demographics table
+ch_acs_demograhics<- ch_acs_detail %>% 
+  select(total_race, white_alone, black, amind_natalas,asian, nathi_pi,hisp) %>% 
+    summarize(CHtotalrace =sum(total_race),
+            totalwhite = sum(white_alone),
+            totalblack = sum(black),
+            totalamind_natalas = sum(amind_natalas),
+            totalasian = sum(asian),
+            totalnathi_pi = sum(nathi_pi),
+            totalhisp = sum(hisp)) %>% 
+    summarize(perc_white= totalwhite/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp), 
+              perc_black= totalblack/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp),  
+              perc_amind_natalas= totalamind_natalas/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp),
+              perc_asian= totalasian/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp),
+              perc_nathi_pi= totalnathi_pi/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp),
+              perc_hisp= totalhisp/ (totalwhite + totalblack + totalamind_natalas + totalasian + totalnathi_pi + totalhisp)
+            )
+
+# creating renter vs owner table
+ch_acs_renters_owners <- ch_acs_detail %>% 
+  select(owners, renters) %>% 
+  summarize(totalrenters =sum(renters),
+            totalowners = sum(owners)) %>% 
+  summarize(perc_renters= totalrenters/(totalowners + totalrenters),
+            perc_owners= totalowners/(totalowners + totalrenters))
+
+
+                                      
